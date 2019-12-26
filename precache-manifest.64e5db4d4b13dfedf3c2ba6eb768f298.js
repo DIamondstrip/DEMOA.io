f447326e7e7fe9bfbe9bf00c47dcbbc9eac059cf
Diamondstrip <943116216@qq.com>
2019-12-26 21:18:58 +0800

    Updates

---
+++ precache-manifest.64e5db4d4b13dfedf3c2ba6eb768f298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e83caa589984ee40fe7e50e326be40c",
+    "url": "/DIamondstrip/DEMOA.io/index.html"
+  },
+  {
+    "revision": "6fd59628ca69af9eca8c",
+    "url": "/DIamondstrip/DEMOA.io/static/css/main.67cf94c5.chunk.css"
+  },
+  {
+    "revision": "9131edc910b9adfa9275",
+    "url": "/DIamondstrip/DEMOA.io/static/js/2.fda1d964.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/DIamondstrip/DEMOA.io/static/js/2.fda1d964.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fd59628ca69af9eca8c",
+    "url": "/DIamondstrip/DEMOA.io/static/js/main.89824da7.chunk.js"
+  },
+  {
+    "revision": "d879c50a4cca90d262f4",
+    "url": "/DIamondstrip/DEMOA.io/static/js/runtime-main.6ca4d1ae.js"
+  }
+]);
